1343a9e734756180cd45173d88cf81092011a0e2
Damian Faron <damian.faron@onet.pl>
2026-01-08 19:31:55 +0100

    set up project structure and add global styles

--- src/pages/SelectMeet.jsx
+++ src/pages/SelectMeet.jsx
@@ -1,8 +1,22 @@
+import Header from '../components/layout/Header/Header';
+import Main from '../components/layout/Main/Main';
+import './SelectMeet.scss';
 const SelectMeet = () => {
   return (
     <>
-      <div>
-        <h1>Calendar </h1>
+      <div className='mainCard'>
+        <div className='itemsCard'>
+          <div className='itemCard'>
+            <p>Select time</p>
+            <h1>South Bay Dental </h1>
+          </div>
+          <div className='itemCard'>
+            <p>Powered by</p>
+            <h2>PaientPop </h2>
+          </div>
+        </div>
+        <Header />
+        <Main />
       </div>
     </>
   );
