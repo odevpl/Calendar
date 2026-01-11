a06e1327cca3c6e57cfac95d643184ef40be7c68
Damian Faron <damian.faron@onet.pl>
2026-01-11 18:06:27 +0100

    add routing for booking in button

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import BookingSummary from './pages/BookingSummary/BookingSummary';
 import SelectMeet from './pages/SelectMeet/SelectMeet';
 
 function App() {
   return (
-    <>
-      <SelectMeet />
-      <BookingSummary />
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' element={<SelectMeet />} />
+        <Route path='/booking/summary' element={<BookingSummary />} />
+      </Routes>
+    </Router>
   );
 }
 
