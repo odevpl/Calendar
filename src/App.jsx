9924718863db25328ad37cc3947bca58f3481de3
Damian Faron <damian.faron@onet.pl>
2026-01-12 20:09:07 +0100

    add basic pages structure and navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import BookingSummary from './pages/BookingSummary/BookingSummary';
 import SelectMeet from './pages/SelectMeet/SelectMeet';
+import Calendar from './pages/calendar/Calendar';
+import Users from './pages/users/Users';
+import NavBar from './components/layout/NavBar/NavBar';
+import UserEdit from './pages/users/edit/UserEdit';
 
 function App() {
   return (
     <Router>
+      <NavBar />
       <Routes>
-        <Route path='/' element={<SelectMeet />} />
+        <Route path='/select-meet' element={<SelectMeet />} />
         <Route path='/booking/summary' element={<BookingSummary />} />
+        <Route path='/calendar' element={<Calendar />} />
+        <Route path='/users' element={<Users />} />
+        <Route path='/users/edit' element={<UserEdit />} />
       </Routes>
     </Router>
   );
