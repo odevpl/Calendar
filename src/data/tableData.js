cef2983c33ee6c6ecab0f081571b33d77916c64d
Damian Faron <damian.faron@onet.pl>
2026-01-19 20:36:16 +0100

    add reusable Table component

---
+++ src/data/tableData.js
@@ -0,0 +1,38 @@
+const tableData = [
+  {
+    name: 'BF Winners',
+    id: 'bf_winners',
+    description: 'Black Friday contest winners',
+    dateCreated: '2021-07-23',
+    createdBy: 'Shelly Wolfe',
+    lastImport: '2 days ago',
+    noOfUsers: '1000',
+    lastRefreshed: '2 days ago',
+    icons: ['refresh', 'edit', 'delete'],
+  },
+
+  {
+    name: 'SF Candidates',
+    id: 'sf_candidates',
+    description: 'SF special offer candidates',
+    dateCreated: '2021-06-30',
+    createdBy: 'John McGrath',
+    lastImport: '5 days ago',
+    noOfUsers: '200',
+    lastRefreshed: '5 days ago',
+    icons: ['refresh', 'edit', 'delete'],
+  },
+  {
+    name: 'CM Winners',
+    id: 'cm_winners',
+    description: 'Cyber Monday contest winners',
+    dateCreated: '2021-01-21',
+    createdBy: 'Ray O',
+    lastImport: '15 days ago',
+    noOfUsers: '1500',
+    lastRefreshed: '8 days ago',
+    icons: ['refresh', 'edit', 'delete'],
+  },
+];
+
+export default tableData;
